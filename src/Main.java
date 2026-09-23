b2298ed73542b817133c5e324787ab3c49d3af1e
Mia <zaitsevavika98@gmail.com>
2026-09-23 19:38:32 +0300

    мой первый коммит:)

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public static void main(String[] args) {
+    int age = 67;
+// age = 67;
+    System.out.println("возраст: " + age);
+
+    if (age >= 18) {
+        System.out.println("пиво можно");
+    } else {
+        System.out.println("пиво нельзя");
+    }
+
+    age = -67;
+    System.out.println(age);
+    System.out.println("age");
+
+    float num7 = 4.133333f;
+    System.out.println(num7);
+
+    char num67 = 'A';
+    String user_name = "vvvvvvvv";
+    System.out.println(user_name);
+
+}
